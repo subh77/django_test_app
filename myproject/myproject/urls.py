b40396e4391cf2d8cb9fd9d71f4004a86bf3371a
subh77 <subhro77@outlook.com>
2021-07-26 17:08:27 +0530

    Added a few styling options (needs modification)

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("app1.urls")),
+    path("app1/", include("app1.urls")),
     path("admin/", admin.site.urls),
 ]
